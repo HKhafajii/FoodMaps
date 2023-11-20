50a5dbd7b0fcdaa68a49a68b2d1db1d7301242ab
HKhafajii <alkhafajihassan@gmail.com>
2023-11-20 13:18:24 -0500

    initial

--- FoodMap/Views/LocationView.swift
+++ FoodMap/Views/LocationView.swift
@@ -12,21 +12,22 @@ import MapKit
 struct LocationView: View {
     
     @EnvironmentObject private var viewModel: LocationsViewModel
-   
+    let maxWidthForIpad: CGFloat = 700
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $viewModel.mapRegion)
+            mapLayer
                 .ignoresSafeArea()
-            
             VStack(spacing: 0) {
                 header
                     .padding()
-                
+                    .frame(maxWidth: maxWidthForIpad)
                 Spacer()
+                locationsPreviewStack
             }
-//            Text($viewModel.mapLocation.name)
         }
-
+        .sheet(item: $viewModel.sheetLocation, onDismiss: nil) { location in
+            LocationsDetailView(location: location)
+        }
     }
 }
 
@@ -64,4 +65,38 @@ extension LocationView {
         }.background(.thickMaterial).cornerRadius(10)
             .shadow(color: .black.opacity(0.3), radius: 20, x: 0 ,y: 15 )
     }
+    private var mapLayer: some View {
+        Map(coordinateRegion: $viewModel.mapRegion,
+            annotationItems: viewModel.locations,
+            annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .scaleEffect(viewModel.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        viewModel.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+    
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(viewModel.locations) { location in
+                if viewModel.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: .black.opacity(0.3), radius: 20)
+                        .frame(maxWidth: maxWidthForIpad)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .transition(.asymmetric(
+                            insertion: .move(edge: .trailing),
+                            removal: .move(edge: .leading))
+                        )
+                }
+                
+            }
+        }
+    }
+    
 }
